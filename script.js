90d0dc379c3065edc26d07240d5621a4ef780f53
DanielHadi <d.suryahadi01@gmail.com>
2024-09-18 15:46:54 +0700

    Updated version, fixed the notification based on first 3 specification

--- script.js
+++ script.js
@@ -23,9 +23,43 @@ const inputValue = () => {
             </span>
         `;
     } else if (parseInt(inputElem.value) < 0){
-        spanElem.innerText = "Please enter a number greater than or equal to 1";
+        spanElem.innerHTML = 
+            `
+            <span id="output" style="
+                background-color: pink;
+                color: rgb(189, 55, 55);
+                text-align: center;
+                padding: 20px;
+                font-size: 30px;
+                margin: 0 auto;
+                display: block;
+                width: 475px;
+                margin-bottom: 25px;
+                border: 2px solid red;
+                "
+            >
+                Please enter a number greater than or equal to 1.
+            </span>
+        `;
     } else if (parseInt(inputElem.value) >= 4000){
-        spanElem.innerText = "Please enter a number less than or equal to 3999";
+        spanElem.innerHTML = 
+            `
+            <span id="output" style="
+                background-color: pink;
+                color: rgb(189, 55, 55);
+                text-align: center;
+                padding: 20px;
+                font-size: 30px;
+                margin: 0 auto;
+                display: block;
+                width: 475px;
+                margin-bottom: 25px;
+                border: 2px solid red;
+                "
+            >
+                Please enter a number less than or equal to 3999.
+            </span>
+        `;
     }
 };
 
