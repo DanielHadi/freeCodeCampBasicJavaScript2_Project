09a45c099149306c715e8b5448da490dc1efafb8
DanielHadi <d.suryahadi01@gmail.com>
2024-09-18 20:54:00 +0700

    Final functional app

--- script.js
+++ script.js
@@ -61,27 +61,56 @@ const inputValue = () => {
             </span>
         `;
     } else {
-        
+        spanElem.innerHTML = 
+            `
+            <span id="output" style="
+                background-color: rgb(93,93,93);
+                color: white;
+                text-align: center;
+                padding: 20px;
+                font-size: 30px;
+                margin: 0 auto;
+                display: block;
+                width: 475px;
+                margin-bottom: 25px;
+                border: 2px solid white;
+                "
+            >
+                ${romanToArabic(parseInt(inputElem.value))}
+            </span>
+        `;
     }
 };
 
 const romanToArabic = (input) => {
-    if (input >= 1){
-        return "I";
-    } else if (input > 4){
-        return "IV";
-    } else if (input > 5){
-        return "V";
-    } else if (input > 9){
-        return "IX";
-    } else if (input > 10){
-        return "X";
-    } else if (input > 40){
-        return "XL";
-    } else if (input > 50){
-        return "L";
-    } else if (input > 90){
-        return "XC";
+    if (input >= 1000){
+        return "M"+romanToArabic(input-1000);
+    } else if (input >= 900){
+        return "CM"+romanToArabic(input-900);
+    } else if (input >= 500){
+        return "D"+romanToArabic(input-500);
+    } else if (input >= 400){
+        return "CD"+romanToArabic(input-400);
+    } else if (input >= 100){
+        return "C"+romanToArabic(input-100);
+    } else if (input >= 90){
+        return "XC"+romanToArabic(input-90);
+    } else if (input >= 50){
+        return "L"+romanToArabic(input-50);
+    } else if (input >= 40){
+        return "XL"+romanToArabic(input-40);
+    } else if (input >= 10){
+        return "X"+romanToArabic(input-10);
+    } else if (input >= 9){
+        return "IX"+romanToArabic(input-9);
+    } else if (input >= 5){
+        return "V"+romanToArabic(input-5);
+    } else if (input >= 4){
+        return "IV"+romanToArabic(input-4);
+    } else if (input >= 1){
+        return "I"+romanToArabic(input-1);
+    } else {
+        return "";
     }
 };
 
